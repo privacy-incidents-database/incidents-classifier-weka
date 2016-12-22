895f2b42f17894d9d1a55d41766084a146e23b54
Pradeep Murukannaiah <pradeepmcl@gmail.com>
2016-12-22 15:52:45 -0500

    Made an update since the getBagOfWords() added null values between sentences

--- src/main/java/edu/ncsu/csc/privacyincidents/nlp/FrrequentTermsComputer.java
+++ src/main/java/edu/ncsu/csc/privacyincidents/nlp/FrrequentTermsComputer.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,6 +20,7 @@ import java.util.TreeMap;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
+import edu.ncsu.csc.privacyincidents.util.StopWordsHandler;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
@@ -27,21 +29,26 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.StringUtils;
 
 public class FrrequentTermsComputer implements AutoCloseable {
 
   private StanfordCoreNLP mNlpPipeline;
+  
+  private StopWordsHandler mStopWordsHandler;
 
   private Map<Integer, File> mFilesMap = new HashMap<Integer, File>();
 
   // For TF, count terms in each document
   private Table<Integer, String, Integer> docTermCounts = HashBasedTable.create();
 
-  public FrrequentTermsComputer() {
+  public FrrequentTermsComputer() throws IOException, URISyntaxException {
     // Open NLP pipleline
     Properties nlpProps = new Properties();
     nlpProps.setProperty("annotators", "tokenize, ssplit, pos, lemma");
     mNlpPipeline = new StanfordCoreNLP(nlpProps);
+    
+    mStopWordsHandler = new StopWordsHandler("/english-stopwords.txt");
   }
 
   public void close() throws Exception {
@@ -111,6 +118,9 @@ public class FrrequentTermsComputer implements AutoCloseable {
     String fileContents = new String(encoded);
 
     List<String> bagOfWords = getBagOfWords(fileContents);
+    
+    // getBagOfWords adds a null value between sentences
+    bagOfWords.removeAll(Collections.singleton(null));
 
     for (String word : bagOfWords) {
       Integer wordCount = docTermCounts.get(fileId, word);
@@ -121,7 +131,9 @@ public class FrrequentTermsComputer implements AutoCloseable {
       }
     }
   }
-
+  
+  // TODO: This method is an exact copy of TfIdfBuilder.getBagOfWords(). Some
+  // refactoring may help reduce code duplication.
   private List<String> getBagOfWords(String text) {
     List<String> words = new ArrayList<String>();
 
@@ -130,13 +142,54 @@ public class FrrequentTermsComputer implements AutoCloseable {
 
     for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
       for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+        // Discard words that only have punctuation, numerals
+        if (StringUtils.isPunct(token.originalText())
+            || StringUtils.isNumeric(token.originalText())) {
+          continue;
+        }
+        
+        // This seems redundant given the following check on isAlpha(), but
+        // somehow this seems to increase prediction accuracy. But, this could
+        // just be due to the fact that I randomly select the negative set and
+        // results might differ slightly from run to run.
+        if (!StringUtils.isAlphanumeric(token.originalText())) {
+          continue;
+        }
+                
+        // Discard non alphabetic words. This may be too restrictive! Consider
+        // adding a list of allowed alphanumeric words such as "49ers"
+        if (!StringUtils.isAlpha(token.originalText())) {
+          // Keep hashtags. This didn't help accuracy; so, commenting it
+          /*if (!token.originalText().startsWith("#")) {
+            continue;
+          }*/
+          continue;
+        }        
+        
+        /*
+         * Long words are likely not natural, e.g., I found instances such as
+         * januaryfebrauary...december in the dataset. Longest common word has
+         * length 20 --
+         * https://web.archive.org/web/20090427054251/http://www.maltron.com/
+         * words/words-longest-modern.html
+         */
+        if (token.originalText().length() > 20) {
+          continue;
+        }
+
         String pos = token.get(PartOfSpeechAnnotation.class);
-        if (pos.startsWith("N") || pos.startsWith("J") || pos.startsWith("V")
-            || pos.startsWith("R")) {
+        // System.out.println(token + ": " + pos);
+        if (pos.startsWith("N") || pos.startsWith("V") 
+            || pos.startsWith("J") || pos.startsWith("R")) {
           String lemma = token.get(LemmaAnnotation.class);
-          words.add(lemma.toLowerCase());
+          
+          // Remove word if it is a stop word
+          if (!mStopWordsHandler.isStopword(lemma)) {
+            words.add(lemma.toLowerCase());
+          }
         }
       }
+      words.add(null);
     }
 
     return words;
